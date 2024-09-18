c9c03c7dfaa9d38b1cdd903e0ec7a7469b50fc65
Rowen21 <167072496+Rowen21@users.noreply.github.com>
2024-09-18 11:18:10 +0200

    Add files via upload

---
+++ Sethmag-banking API/src/main/java/com/example/Sethmag_bank/SethmagBankApplication.java
@@ -0,0 +1,13 @@
+package com.example.Sethmag_bank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SethmagBankApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SethmagBankApplication.class, args);
+	}
+
+}
---
+++ Sethmag-banking API/src/test/java/com/example/Sethmag_bank/SethmagBankApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Sethmag_bank;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SethmagBankApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
